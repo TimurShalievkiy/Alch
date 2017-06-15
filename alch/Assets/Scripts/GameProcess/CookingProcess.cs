20279facc52effb802eafcefc341a8453297ee9e
lKORENl <korenanon@gmail.com>
2017-06-15 15:15:12 +0300

    Work on a prototype

---
+++ alch/Assets/Scripts/GameProcess/CookingProcess.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingProcess : MonoBehaviour {
+    public GameObject gridSequence;
+    public GameObject ConteinerIngr;
+	// Use this for initialization
+	void Start () {
+        LoadIngrInSequence();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void LoadIngrInSequence()
+    {
+        
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject a = ConteinerIngr;
+            GameObject.Instantiate(a);//.SetParent(gridSequence.transform);
+            a.transform.SetParent(gridSequence.transform);
+
+        }
+    }
+}
