a826c6d15f1a57caca3b2dc7686b8fd4f9ef03bc
lKORENl <korenanon@gmail.com>
2017-07-30 01:30:52 +0300

    scrollMenu

---
+++ alch/Assets/Resources/Scripts/Interface/Menu/MenuScroller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuScroller : EventTrigger
+{
+
+    float oldPos = 0;
+    float current;
+    public override void OnBeginDrag(PointerEventData data)
+    {
+        Debug.Log("begin drag");
+        oldPos = data.position.x;
+    }
+    public override void OnDrag(PointerEventData data)
+    {
+        Debug.Log("drag");
+        
+ 
+            current = data.position.x - oldPos;
+        if (current > 200)
+            current = 200;
+        if (current < -200)
+            current = -200;
+   
+
+            this.transform.GetChild(0).transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(0).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(1).transform.position.y);
+            this.transform.GetChild(0).transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(1).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(2).transform.position.y);
+            this.transform.GetChild(0).transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(2).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(3).transform.position.y);
+            this.transform.GetChild(0).transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(3).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(4).transform.position.y);
+            this.transform.GetChild(0).transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(4).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(5).transform.position.y);
+            this.transform.GetChild(0).transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(5).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(6).transform.position.y);
+            this.transform.GetChild(0).transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(6).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(7).transform.position.y);
+
+            oldPos = data.position.x;       
+    }
+    public override void OnEndDrag(PointerEventData data)
+    {
+        oldPos = 0;
+        Debug.Log("OnEndDrag called.");
+    }
+
+
+}
---
+++ alch/Assets/Resources/Scripts/Interface/Menu/TrigerZoneMenuButton.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrigerZoneMenuButton : MonoBehaviour
+{
+
+    public GameObject NextPosition;
+
+
+    public GameObject LeftPosition;
+    public GameObject RightPosition;
+
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+
+    //    if (collision.transform.GetComponent<MenuButton>() && curentTransportName != collision.name)
+    //    {
+    //        Debug.Log("triger " + this.name);
+    //        curentTransportName = collision.name;
+    //        collision.transform.position = NextPosition.transform.position;
+    //    }
+    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.GetComponent<MenuButton>())
+        {
+            collision.transform.localScale = new Vector2(1.5f, 1.5f);
+            switch (collision.name)
+            {
+                case "1":
+                    this.transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(5).position.x - 300, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(2).position.x + 300, this.transform.GetChild(0).position.y);
+
+                    break;
+                case "2":
+                    this.transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(6).position.x - 300, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(3).position.x + 300, this.transform.GetChild(0).position.y);
+
+                    break;
+                case "3":
+                    this.transform.GetChild(6).transform.position = new Vector2( this.transform.GetChild(0).position.x -300, this.transform.GetChild(0).position.y) ;
+                    this.transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(4).position.x + 300, this.transform.GetChild(0).position.y);
+                    break;
+                case "4":
+                    this.transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(1).position.x - 300, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(5).position.x + 300, this.transform.GetChild(0).position.y);
+
+                    break;
+                case "5":
+                    this.transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(6).position.x + 300, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(2).position.x - 300, this.transform.GetChild(0).position.y);
+
+                    break;
+                case "6":
+                    this.transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).position.x + 300, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(3).position.x - 300, this.transform.GetChild(0).position.y);
+
+                    break;
+                case "7":
+                    this.transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(1).position.x + 300, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(4).position.x - 300, this.transform.GetChild(0).position.y);
+
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.GetComponent<MenuButton>())
+        {
+            collision.transform.localScale = new Vector2(1, 1);
+
+        }
+    }
+}
