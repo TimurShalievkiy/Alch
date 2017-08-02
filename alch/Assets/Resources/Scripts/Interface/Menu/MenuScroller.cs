c5595508b98a328bb40776004b3c7cea0ebe9e18
lKORENl <korenanon@gmail.com>
2017-08-02 21:33:31 +0300

    Menu

--- alch/Assets/Resources/Scripts/Interface/Menu/MenuScroller.cs
+++ alch/Assets/Resources/Scripts/Interface/Menu/MenuScroller.cs
@@ -5,40 +5,144 @@ using UnityEngine.EventSystems;
 
 public class MenuScroller : EventTrigger
 {
-
+    //включена ли обработка инерции
+    bool doInertia;
+    //направление инерции
+    bool way;
+    //предыдущая позиция тача
     float oldPos = 0;
+    //разница между предыдущей и нынешней позицией тача
     float current;
+    //переменная для замедления в прощете инерции
+    float antiInert;
+
+
+    //обработка инерции
+    public void FixedUpdate()
+    {
+        //если инерция включена
+        if (doInertia)
+        {
+            //непревышает ли смещение максимально допустим границ
+            if (current > 75)
+                current = 75;
+            if (current < -75)
+                current = -75;
+            //если инерция в правую сторону
+            if (way)
+            {
+                //отнимаем от смещения переменную замедления
+                current -= antiInert;
+                //увеличиваем переменную замедления 
+                antiInert += Time.deltaTime * 5;
+
+                //смещение элементов меню
+                this.transform.GetChild(0).transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(0).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(1).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(1).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(2).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(2).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(3).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(3).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(4).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(4).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(5).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(5).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(6).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(6).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(7).transform.position.y);
+
+                //если смещение меньше переменной замедления 
+                if (current < antiInert)
+                {
+                    //обнуляем переменную замедления
+                    antiInert = 0;
+                    //останавливаем инецию
+                    doInertia = false;
+                }
+            }
+            //если направление инерции влево
+            else
+            {
+                //прибавляем переменную замедления к смещению
+                current += antiInert;
+                //увеличиваем переменную замедления 
+                antiInert += Time.deltaTime * 5;
+
+                //смещение элементов меню
+                this.transform.GetChild(0).transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(0).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(1).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(1).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(2).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(2).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(3).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(3).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(4).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(4).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(5).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(5).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(6).transform.position.y);
+                this.transform.GetChild(0).transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(6).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(7).transform.position.y);
+
+                //если смещение превысило 0
+                if (current > 0)
+                {
+                    //обнуляем переменную замедления
+                    antiInert = 0;
+                    //останавливаем инецию
+                    doInertia = false;
+                }
+            }
+        }
+    }
+
+
+    //при начале перетаскивания
     public override void OnBeginDrag(PointerEventData data)
     {
-        Debug.Log("begin drag");
+        //останавливаем инерцию
+        doInertia = false;
+        //обнуляем переменную замедления
+        antiInert = 0;
+        //задаем предыдущую позицию смещения тача
         oldPos = data.position.x;
     }
+
+    //в момент перетаскивания
     public override void OnDrag(PointerEventData data)
     {
-        Debug.Log("drag");
-        
- 
-            current = data.position.x - oldPos;
-        if (current > 200)
-            current = 200;
-        if (current < -200)
-            current = -200;
-   
+        //выщитываем разницу между предыдущей и нынешней позицией тача
+        current = data.position.x - oldPos;
 
-            this.transform.GetChild(0).transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(0).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(1).transform.position.y);
-            this.transform.GetChild(0).transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(1).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(2).transform.position.y);
-            this.transform.GetChild(0).transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(2).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(3).transform.position.y);
-            this.transform.GetChild(0).transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(3).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(4).transform.position.y);
-            this.transform.GetChild(0).transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(4).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(5).transform.position.y);
-            this.transform.GetChild(0).transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(5).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(6).transform.position.y);
-            this.transform.GetChild(0).transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(6).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(7).transform.position.y);
+        //проверяем на выход за допустимый диапазон
+        if (current > 75)
+            current = 75;
+        if (current < -75)
+            current = -75;
 
-            oldPos = data.position.x;       
+        //смещаем элементы меню
+        this.transform.GetChild(0).transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(0).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(1).transform.position.y);
+        this.transform.GetChild(0).transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(1).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(2).transform.position.y);
+        this.transform.GetChild(0).transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(2).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(3).transform.position.y);
+        this.transform.GetChild(0).transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(3).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(4).transform.position.y);
+        this.transform.GetChild(0).transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(4).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(5).transform.position.y);
+        this.transform.GetChild(0).transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(5).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(6).transform.position.y);
+        this.transform.GetChild(0).transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(0).transform.GetChild(6).transform.position.x + current, this.transform.GetChild(0).transform.GetChild(7).transform.position.y);
+
+        //переопреназначаем старую позициюж
+        oldPos = data.position.x;
+
     }
+
+    //по завершении перетаскивания
     public override void OnEndDrag(PointerEventData data)
     {
+        //проверяем на выход за допустимый диапазон
+        if (current > 75)
+            current = 75;
+        if (current < -75)
+            current = -75;
+
+        //уменьшаем в два раза значение разницы 
+        current = (data.position.x - oldPos) / 2;
+
+        //определяем в какую сторуну идет инерция
+        if (current > 0)
+            way = true;
+        else if (current < 0)
+            way = false;
         oldPos = 0;
-        Debug.Log("OnEndDrag called.");
+
+        //запускаем инерцию
+        doInertia = true;
+
     }
 
 
--- alch/Assets/Resources/Scripts/Interface/Menu/TrigerZoneMenuButton.cs
+++ alch/Assets/Resources/Scripts/Interface/Menu/TrigerZoneMenuButton.cs
@@ -5,76 +5,63 @@ using UnityEngine;
 public class TrigerZoneMenuButton : MonoBehaviour
 {
 
-    public GameObject NextPosition;
 
-
-    public GameObject LeftPosition;
-    public GameObject RightPosition;
-
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-
-    //    if (collision.transform.GetComponent<MenuButton>() && curentTransportName != collision.name)
-    //    {
-    //        Debug.Log("triger " + this.name);
-    //        curentTransportName = collision.name;
-    //        collision.transform.position = NextPosition.transform.position;
-    //    }
-    //}
+    //при входе элемента в тригер зону
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //проверяем есть ли у этого элемента скрипт MenuButton
         if (collision.transform.GetComponent<MenuButton>())
         {
+            //переменная растояния между кнопками меню
+            float p = collision.bounds.size.x * 2.5f;
+
+            //увеличение скейла кнопки
             collision.transform.localScale = new Vector2(1.5f, 1.5f);
+         
+            //в зависимости какая кнопка зашла в тригер перемещаем крайние на нужные позиции
             switch (collision.name)
             {
                 case "1":
-                    this.transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(5).position.x - 300, this.transform.GetChild(0).position.y);
-                    this.transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(2).position.x + 300, this.transform.GetChild(0).position.y);
-
+                    this.transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(5).position.x - p, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(2).position.x + p, this.transform.GetChild(0).position.y);
                     break;
                 case "2":
-                    this.transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(6).position.x - 300, this.transform.GetChild(0).position.y);
-                    this.transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(3).position.x + 300, this.transform.GetChild(0).position.y);
-
+                    this.transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(6).position.x - p, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(4).transform.position = new Vector2(this.transform.GetChild(3).position.x + p, this.transform.GetChild(0).position.y);
                     break;
                 case "3":
-                    this.transform.GetChild(6).transform.position = new Vector2( this.transform.GetChild(0).position.x -300, this.transform.GetChild(0).position.y) ;
-                    this.transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(4).position.x + 300, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(0).position.x - p, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(5).transform.position = new Vector2(this.transform.GetChild(4).position.x + p, this.transform.GetChild(0).position.y);
                     break;
                 case "4":
-                    this.transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(1).position.x - 300, this.transform.GetChild(0).position.y);
-                    this.transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(5).position.x + 300, this.transform.GetChild(0).position.y);
-
+                    this.transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(1).position.x - p, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(6).transform.position = new Vector2(this.transform.GetChild(5).position.x + p, this.transform.GetChild(0).position.y);
                     break;
                 case "5":
-                    this.transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(6).position.x + 300, this.transform.GetChild(0).position.y);
-                    this.transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(2).position.x - 300, this.transform.GetChild(0).position.y);
-
+                    this.transform.GetChild(0).transform.position = new Vector2(this.transform.GetChild(6).position.x + p, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(2).position.x - p, this.transform.GetChild(0).position.y);              
                     break;
                 case "6":
-                    this.transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).position.x + 300, this.transform.GetChild(0).position.y);
-                    this.transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(3).position.x - 300, this.transform.GetChild(0).position.y);
-
+                    this.transform.GetChild(1).transform.position = new Vector2(this.transform.GetChild(0).position.x + p, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(3).position.x - p, this.transform.GetChild(0).position.y);
                     break;
                 case "7":
-                    this.transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(1).position.x + 300, this.transform.GetChild(0).position.y);
-                    this.transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(4).position.x - 300, this.transform.GetChild(0).position.y);
-
-                    break;
-                default:
+                    this.transform.GetChild(2).transform.position = new Vector2(this.transform.GetChild(1).position.x + p, this.transform.GetChild(0).position.y);
+                    this.transform.GetChild(3).transform.position = new Vector2(this.transform.GetChild(4).position.x - p, this.transform.GetChild(0).position.y);
                     break;
             }
 
-        }
-        
 
+        }      
     }
+
+    //при выходе из тригера
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //проверяем есть ли у этого элемента скрипт MenuButton
         if (collision.transform.GetComponent<MenuButton>())
         {
+            //возвращаем скейл в нормальное положение
             collision.transform.localScale = new Vector2(1, 1);
 
         }
